821f93081fa86138d9b88bc1d361a5057c889ea5
Ismael Nasciemnto <117104139+ismaelvieira12@users.noreply.github.com>
2023-06-02 08:36:28 -0300

    alteração am casa

--- meu_script.js
+++ meu_script.js
@@ -14,59 +14,71 @@
 
 
 const cellElements = document.querySelectorAll("[data-cell]");
-// const board = document.querySelector("[data-board]");
+const board = document.querySelector("[data-board]");   
 // const msg = document.querySelector('[data-msg]');
 // const mensage = document.querySelector('data-mensage]');
 
 let isCircleTurm;
 
 // FUNCÃO PARA STARTAR O GAME
-// const startGame = () => {
+const startGame = () => {
 
-//     // PARA QUE O (X) COMERCE O JOGO, TEMOS QUE COLOCAR O (CIRCULO) COMO 
-//     // FALSO OU DEIXAR ELE FALSO DESDE DA CRIAÇÃO DA  VARIÁVEL E CETAR O (X)
-//     // COMO PRINCIPAL.
+    // PARA QUE O (X) COMERCE O JOGO, TEMOS QUE COLOCAR O (CIRCULO) COMO 
+    // FALSO OU DEIXAR ELE FALSO DESDE DA CRIAÇÃO DA  VARIÁVEL E CETAR O (X)
+    // COMO PRINCIPAL.
 
-// }
+    for (const cell of cellElements) {
+        cell.addEventListener("click", handleClick, {once: true});
+    };    
+    isCircleTurm = false;
+
+    board.classList.add('x');
+};
 
 //CRIANDO A MARCA SE VAI SER (X) OU (CIRCLE)
-// const placeMark = (cell, classToAdd) => {
-    
-// }
+const placeMark = (cell, classToAdd) => {
+    cell.classList.add(classToAdd);
+}
 
 // CHECAR POR VITÓRIA
-// const combinacao = [
-//     // PRIMEIRA SEGUNDA E TERCEIRA LINHA
-//     [0, 1, 2],
-//     [3, 4, 5],
-//     [6, 7, 8],
+const combinacao = [
+    // PRIMEIRA SEGUNDA E TERCEIRA LINHA
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
 
-//     // PRIMEIRA SEGUNDA E TERCEIRA COLUNA
-//     [0, 3, 6],
-//     [1, 4, 7],
-//     [2, 5, 8],
+    // PRIMEIRA SEGUNDA E TERCEIRA COLUNA
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
 
-//     //DIAGONAIS
-//     [0, 4, 8],
-//     [2, 4, 6],
+    //DIAGONAIS
+    [0, 4, 8],
+    [2, 4, 6],
 
-// ];
+];
 
 //CHECAR POR VITÓRIA
 // JOGADOR ATUAL corrent player
-// const jogadorAtual = (currentPlayer) => {
+const checkForwin = (currentPlayer) => {
     
         // ELE VAI CHECAR SE ALGUMA DAS COMBINAÇÕES FORAM PREENCHIDAS PELO JOGADOR ATUAL
 
-// };
+};
 //MUDAR O SIMBULO DA BOARD swapTurn
-// const mudaSimbulo = () => {
+const swapTurn = () => {
+    isCircleTurm = !isCircleTurm;
 
+    board.classList.remove('circle');
+    board.classList.remove('x');
 
     // UMA CONDIÇÃO PARA SABER SE É A HORA DO CIRCULO
-    
-    
-// }
+    if (isCircleTurm) {
+        board.classList.add('circle');
+    } else{
+        board.classList.add('x');
+    }
+}
 
 // FIM DO JOGO E VERIFICAR POR EMPATE
 
@@ -75,22 +87,21 @@ const handleClick = (e) => {
     const cell = e.target;
     const classToAdd = isCircleTurm ? 'circle' : 'x';
 
-    cell.classList.add(classToAdd);
-
-    //2° Checar por vitória
+    placeMark(cell, classToAdd);
     
-    //3° Verificar por empate
+    //2° Mudar o SIMBULO (x) ou (circle)
+    swapTurn();
 
-    //4° Mudar o SIMBULO (x) ou (circle)
+    //3° Checar por vitória
+    
+    //4° Verificar por empate
+
 };
 
-for (const cell of cellElements) {
-    cell.addEventListener("Click", handleClick, {once: true});
-};
 
 
 
-// startGame();
+startGame();
 
 
 
