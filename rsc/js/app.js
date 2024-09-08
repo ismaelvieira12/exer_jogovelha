0fd4899ab5256a0f16ba03f9dab34769a205f571
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2024-09-08 13:38:22 -0300

    finished part of the

---
+++ rsc/js/app.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector("#btn");
+const mensage = document.querySelector(".winning-message");
+btn.addEventListener('click', () => {
+    mensage.style.display="none";
+})
